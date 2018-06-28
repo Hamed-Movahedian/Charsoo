029ef5e00ef0a62169d25221a7b1546e01d9c6ae
HoseinNajafi <najafi.h.ir@gmail.com>
2018-06-29 00:27:03 +0430

    Menu

---
+++ Assets/RTFixText.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using ArabicSupport;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RTFixText : MonoBehaviour
+{
+    public Text TargetText;
+    private Text _text;
+    private string _content;
+
+    public void Start()
+    {
+        _text = GetComponent<Text>();
+    }
+
+    private void OnEnable()
+    {
+        StartCoroutine(FixText());
+
+    }
+
+
+    private void OnDisable()
+    {
+        StopCoroutine(FixText());
+
+    }
+    
+
+    private IEnumerator FixText()
+    {
+        while (true)
+        {
+           _text.text = ArabicFixer.Fix(TargetText.text);
+            yield return new WaitForSeconds(0.1f);
+        }
+    }
+
+}
