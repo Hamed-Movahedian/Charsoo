43af7f803aa141676e4ac0c10dd7f70d530f6e58
HoseinNajafi <najafi.h.ir@gmail.com>
2018-08-24 13:19:10 +0430

    no message

---
+++ Assets/Scripts/Generator/CategorySelectionWindow.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class CategorySelectionWindow : UIMenuItemList
+{
+
+    public void Refresh()
+    {
+        var categories = LocalDBController.Table<Category>().ToList();
+        categories.Sort((p1, p2) => p1.Row.CompareTo(p2.Row));
+        UpdateItems(categories.Cast<object>());
+    }
+}
---
+++ Assets/Scripts/Generator/LocalPuzzlesSelectionWindow.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class LocalPuzzlesSelectionWindow : UIMenuItemList
+{
+    public int CategoryID { get; set; }
+
+    public void Refresh()
+    {
+        var categories = LocalDBController.Table<Puzzle>().SqlWhere(p=>p.CategoryID==CategoryID).ToList();
+        categories.Sort((p1, p2) => p1.Row.CompareTo(p2.Row));
+        UpdateItems(categories.Cast<object>());
+    }
+}
