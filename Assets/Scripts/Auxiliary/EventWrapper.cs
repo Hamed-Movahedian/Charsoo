39946a84e735f067a22b7ed23bcc1d0d0e320c51
Hamed Movahedian <movahedian.h@gmail.com>
2018-09-09 19:29:35 +0430

    no message

---
+++ Assets/Scripts/Auxiliary/EventWrapper.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class EventWrapper : MonoBehaviour
+{
+    public UnityEvent Action;
+
+    public void RunAction()
+    {
+        Action.Invoke();
+    }
+}
