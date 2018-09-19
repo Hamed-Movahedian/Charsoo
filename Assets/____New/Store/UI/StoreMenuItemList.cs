71e994922204ed27979b2acfffafe3379674e5b5
HoseinNajafi <najafi.h.ir@gmail.com>
2018-09-19 12:26:48 +0430

    no message

---
+++ Assets/____New/Store/UI/StoreMenuItemList.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StoreMenuItemList : UIMenuItemList
+{
+    [System.Serializable]
+    public class StoreItem
+    {
+        public string ItemTitle;
+        public int Price;
+        public Sprite Icon;
+    }
+
+    public List<StoreItem> StoreItems;
+
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/____New/Store/UI/UIStoreMenuItem.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIStoreMenuItem : UIMenuItem
+{
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    protected override void Refresh(object data)
+    {
+        throw new System.NotImplementedException();
+    }
+}
