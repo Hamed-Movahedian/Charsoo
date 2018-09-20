6b80c2da362107c59c634fe8ddd4fd45a1b947df
HoseinNajafi <najafi.h.ir@gmail.com>
2018-09-20 13:16:22 +0430

    no message

---
+++ Assets/Scripts/Editor/ExportWordsetToText.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class ExportWordsetToText : ScriptableWizard
+{
+    public string FileAddress = "D://WordSets.txt";
+    [MenuItem("Word Game/Export To Text")]
+    static void CreateWizard()
+    {
+        DisplayWizard<ExportWordsetToText>("Export Words", "Export");
+    }
+
+
+    void OnWizardCreate()
+    {
+        string Allfile = "";
+        foreach (PuzzleComponent puzzle in FindObjectsOfType<PuzzleComponent>())
+        {
+            string p ="اشاره: "+ puzzle.Clue+",";
+            WordSet ws=new WordSet();
+            JsonUtility.FromJsonOverwrite(StringCompressor.DecompressString(puzzle.PuzzleData.Content), ws);
+            foreach (SWord word in ws.Words)
+            {
+                p += word.Name + ",";
+            }
+            p += "\n";
+            Allfile += p;
+        }
+        System.IO.File.WriteAllText(FileAddress, Allfile);
+    }
+
+
+}
