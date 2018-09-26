33074739c8f1da730d4b56b4a779a7d7743c60d4
HoseinNajafi <najafi.h.ir@gmail.com>
2018-09-26 16:50:52 +0330

    no message

---
+++ Assets/Scripts/UI/Tools/PlayerNameSetter.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using ArabicSupport;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerNameSetter : MonoBehaviour
+{
+    private Text _text;
+    public void OnEnable()
+    {
+        PlayerInfo info = LocalDBController.Table<PlayerInfo>().FirstOrDefault();
+        GetComponent<Text>().text = ArabicFixer.Fix(info.PlayerID+"   "+ info.Name);
+    }
+
+}
