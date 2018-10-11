f707539fa113aa8d6ddc740abd2463708cacc1ce
Hamed Movahedian <movahedian.h@gmail.com>
2018-10-11 11:12:07 +0330

    no message

---
+++ Assets/Scripts/Singletons/DeepLinkController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeepLinkController : MonoBehaviour
+{
+    public string Data { get; set; }
+    public string Action { get; set; }
+
+    public void GetDeepLinkInfo()
+    {
+        if (Application.platform==RuntimePlatform.Android)
+        {
+            AndroidJavaClass UnityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+            AndroidJavaObject currentActivity = UnityPlayer.GetStatic<AndroidJavaObject>("currentActivity");
+            if (currentActivity == null)
+            {
+                Data = "Error : currentActivity = null";
+                return;
+            }
+
+            AndroidJavaObject intent = currentActivity.Call<AndroidJavaObject>("getIntent");
+            if (intent == null)
+            {
+                Data = "Error : intent = null";
+                return;
+            }
+
+            //Action = intent.Call<string>("getAction");
+            Data = intent.Call<string>("getDataString"); 
+        }
+    }
+}
