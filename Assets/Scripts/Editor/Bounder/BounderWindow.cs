a8b248107ee3ded3b0ee016692787f2f72e2badc
DESKTOP-SM4SNDC\Hamed <Movahedian.h@gmail.com>
2018-10-14 14:57:57 +0330

    no message

--- Assets/Scripts/Editor/Bounder/BounderWindow.cs
+++ Assets/Scripts/Editor/Bounder/BounderWindow.cs
@@ -53,9 +53,10 @@ public class BounderWindow : EditorWindow
     #region Privates
 
     private List<string> _boundText = new List<string>();
-    private List<string> _menuItems = new List<string>();
+    private List<Type> _boundTypes = new List<Type>();
+
     private List<MemberInfo> _memberInfos = new List<MemberInfo>();
-    private List<Type> _types = new List<Type>();
+
     private GameObject _targetGO;
     private Dictionary<string, Type> _typeDic = new Dictionary<string, Type>();
     private Vector2 _scrollPos;
@@ -65,8 +66,9 @@ public class BounderWindow : EditorWindow
     {
         typeof(Int32),typeof(Boolean),typeof(string),typeof(Single)
     };
+
     #endregion
-    
+
     #region Window Functions
 
     [MenuItem("Test/Bounder")]
@@ -87,51 +89,25 @@ public class BounderWindow : EditorWindow
     private void UpdateList()
     {
         _search = "";
-        if (_boundText.Count == 0)
+
+        if (_boundTypes.Count == 0)
         {
-            if (_targetGO == null)
-            {
-                Reset();
-                return;
-            }
-
-            _types = _targetGO
-                .GetComponents<Component>()
-                .Select(c => c.GetType())
-                .ToList();
-
-            _types.Insert(0, typeof(GameObject));
-
-            _types.ForEach(t => _typeDic[t.Name] = t);
-
-            _menuItems = _types
-                .Select(t => GetTypeFrindlyName(t))
-                .ToList();
+            _memberInfos.Clear();
+            return;
         }
-        else
-        {
-            var finalType = GetFinalType();
 
-            _memberInfos = finalType
-                .GetMembers(BindingFlags.Public | BindingFlags.Instance)
-                .Where(m => IsValidMember(m))
-                .Where(m => GetMemberType(m) != typeof(void))
-                .OrderBy(m => m.Name)
-                .OrderBy(m => GetTypeDistance(finalType, m.DeclaringType))
-                .ToList();
+        var finalType = _boundTypes[_boundTypes.Count - 1];
 
-            _menuItems = _memberInfos
-                .Select(m => GetMemberFrindlyName(m))
-                .ToList();
-        }
+        _memberInfos = finalType
+            .GetMembers(BindingFlags.Public | BindingFlags.Instance)
+            .Where(m => IsValidMember(m))
+            .OrderBy(m => m.Name)
+            .OrderBy(m => GetTypeDistance(finalType, m.DeclaringType))
+            .ToList();
     }
     #endregion
 
     #region Utils
-    private bool IsDisplayingMembers()
-    {
-        return _boundText.Count != 0;
-    }
     private int GetTypeDistance(Type finalType, Type type)
     {
         int i = 0;
@@ -209,59 +185,7 @@ public class BounderWindow : EditorWindow
 
     #endregion
 
-    #region GetFinalType
-    private Type GetFinalType()
-    {
-        if (_boundText.Count == 0)
-            return null;
-
-        var type = _typeDic[_boundText[0]];
-
-        for (int i = 1; i < _boundText.Count; i++)
-        {
-            var memberInfo = GetMember(type, _boundText[i]);
-            type = GetMemberType(memberInfo);
-        }
-
-        return type;
-    }
-
-    #endregion
-    
     #region Frindly Names
-    private string GetMemberFrindlyName(MemberInfo memberInfo)
-    {
-        switch (memberInfo.MemberType)
-        {
-            case MemberTypes.Field:
-                var fieldInfo = (FieldInfo)memberInfo;
-                return $"{fieldInfo.Name,-35} {fieldInfo.FieldType.Name}";
-
-            case MemberTypes.Property:
-                var propertyInfo = memberInfo as PropertyInfo;
-
-                return $"{propertyInfo.Name,-35} {propertyInfo.PropertyType.Name}";
-
-            case MemberTypes.Method:
-                var methodInfo = memberInfo as MethodInfo;
-
-                var frindlyName = $"{methodInfo.Name,-35} {methodInfo.ReturnType.Name}(";
-
-                var parameters = methodInfo.GetParameters();
-
-                for (var i = 0; i < parameters.Length; i++)
-                {
-                    var parameterInfo = parameters[i];
-                    frindlyName += $"{parameterInfo.ParameterType.Name} {parameterInfo.Name}{(i == parameters.Length - 1 ? "" : ",  ")} ";
-                }
-
-                return frindlyName + ")";
-
-            default:
-                throw new ArgumentOutOfRangeException();
-        }
-    }
-
     private string GetTypeFrindlyName(Type type)
     {
         return $"{type.Name,-35} ({type.FullName})";
@@ -272,8 +196,6 @@ public class BounderWindow : EditorWindow
     private void Reset()
     {
         _boundText.Clear();
-        _menuItems.Clear();
-        _types.Clear();
         _memberInfos.Clear();
     }
     #endregion
@@ -281,21 +203,16 @@ public class BounderWindow : EditorWindow
     #region AddLevel
     private void AddLevel(int i)
     {
-        if (_boundText.Count == 0)
-        {
-            _boundText.Add(_types[i].Name);
-        }
+
+        string text;
+
+        if (_memberInfos[i].MemberType == MemberTypes.Method)
+            text = _memberInfos[i].Name + "()";
         else
-        {
-            string text;
+            text = _memberInfos[i].Name;
 
-            if (_memberInfos[i].MemberType == MemberTypes.Method)
-                text = _memberInfos[i].Name + "()";
-            else
-                text = _memberInfos[i].Name;
-
-            _boundText.Add(text);
-        }
+        _boundText.Add(text);
+        _boundTypes.Add(GetMemberType(_memberInfos[i]));
 
         UpdateList();
     }
@@ -306,25 +223,29 @@ public class BounderWindow : EditorWindow
     private void Back()
     {
         _boundText.RemoveAt(_boundText.Count - 1);
-
+        _boundTypes.RemoveAt(_boundTypes.Count-1);
         UpdateList();
     }
 
     #endregion
 
-    #region GUI
+    // ********************* GUI
+
+    #region OnGUI
     void OnGUI()
     {
         GUILayout.Space(5);
 
         #region TargetGameObject
+
         // ********************   TargetGameObject
-        var gameObject = (GameObject)EditorGUILayout.ObjectField("Game Object", _targetGO, typeof(GameObject), true);
+        var gameObject = (GameObject) EditorGUILayout.ObjectField("Game Object", _targetGO, typeof(GameObject), true);
 
         if (_targetGO != gameObject)
         {
             _targetGO = gameObject;
-            Reset();
+            _boundTypes.Clear();
+            _boundText.Clear();
             UpdateList();
         }
 
@@ -333,33 +254,80 @@ public class BounderWindow : EditorWindow
 
         #endregion
 
-        #region Serialize text and back
-        // ********************    Serialize text and back
-        if (_boundText.Count > 0)
+        #region Component
+
+        // target game object components + GameObject
+        var types = _targetGO.GetComponents<Component>()
+            .Select(c => c.GetType())
+            .ToList();
+
+        types.Insert(0, typeof(GameObject));
+
+        // Select _boundTypes first item
+        int index = 0;
+
+        if (_boundTypes.Count == 0)
         {
-            GUILayout.BeginHorizontal();
+            _boundTypes.Add(null);
+        }
+        
+        index = types.IndexOf(_boundTypes[0]);
 
-            GUILayout.Label(_boundText.Aggregate((a, b) => a + "." + b), BindStringStyle);
+        if (index == -1)
+            index = types.Count - 1;
 
-            //GUILayout.FlexibleSpace();
-            if (GUILayout.Button(" ◄ ", GUILayout.ExpandHeight(true)))
-                Back();
+        index = EditorGUILayout.Popup(new GUIContent("Component"), index, types.Select(t => t.Name).ToArray());
 
-            GUILayout.EndHorizontal();
+        if (types[index] != _boundTypes[0])
+        {
+            _boundTypes.Clear();
+            _boundTypes.Add(types[index]);
+
+            _boundText.Clear();
+            _boundText.Add(types[index].Name);
+
+
+            UpdateList();
         }
 
-        EditorUtils.BoldSeparator();
         #endregion
-        
+
+        #region Serialize text and back
+
+        // ********************    Serialize text and back
+
+        GUILayout.BeginHorizontal();
+
+        GUILayout.Label(_boundText.Aggregate((a, b) => a + "." + b), BindStringStyle);
+
+        if (_boundText.Count > 1)
+            if (GUILayout.Button(" ◄ ", GUILayout.ExpandWidth(false), GUILayout.ExpandHeight(true)))
+                Back();
+
+        GUILayout.EndHorizontal();
+
+        EditorUtils.BoldSeparator();
+
+        #endregion
+
         #region SEARCH
+
         // *******************   SEARCH
-        if (Event.current.keyCode == KeyCode.DownArrow) { GUI.SetNextControlName("SearchToolbar"); }
-        if (Event.current.keyCode == KeyCode.UpArrow) { GUI.SetNextControlName("SearchToolbar"); }
+        if (Event.current.keyCode == KeyCode.DownArrow)
+        {
+            GUI.SetNextControlName("SearchToolbar");
+        }
+
+        if (Event.current.keyCode == KeyCode.UpArrow)
+        {
+            GUI.SetNextControlName("SearchToolbar");
+        }
+
         GUILayout.BeginHorizontal();
         {
             GUI.SetNextControlName("SearchToolbar");
             _search = EditorGUILayout.TextField(_search, SearchTextFieldStyle);
-            if (GUILayout.Button("", (GUIStyle)"ToolbarSeachCancelButton"))
+            if (GUILayout.Button("", (GUIStyle) "ToolbarSeachCancelButton"))
             {
                 _search = string.Empty;
                 GUI.SetNextControlName("SearchToolbar");
@@ -367,99 +335,87 @@ public class BounderWindow : EditorWindow
         }
         GUILayout.EndHorizontal();
         EditorUtils.BoldSeparator();
+
         #endregion
-        
+
         #region Item List
+
         // ********************** Item List
         Type lastItemType = null;
 
         _scrollPos = GUILayout.BeginScrollView(_scrollPos);
-        for (int i = 0; i < _menuItems.Count; i++)
+        for (int i = 0; i < _memberInfos.Count; i++)
         {
             if (_search != "")
-                if (!_menuItems[i].StartsWith(_search, StringComparison.CurrentCultureIgnoreCase))
+                if (!_memberInfos[i].Name.StartsWith(_search, StringComparison.CurrentCultureIgnoreCase))
                     continue;
 
-            if (IsDisplayingMembers())
-            {
-                if (lastItemType != _memberInfos[i].DeclaringType && lastItemType != null)
-                    EditorUtils.BoldSeparator();
+            if (lastItemType != _memberInfos[i].DeclaringType && lastItemType != null)
+                EditorUtils.BoldSeparator();
 
-                lastItemType = _memberInfos[i].DeclaringType;
-            }
+            lastItemType = _memberInfos[i].DeclaringType;
 
             DrawItem(i);
 
             var lastRect = GUILayoutUtility.GetLastRect();
-
             if (GUI.Button(lastRect, GUIContent.none, GUIStyle.none))
             {
                 AddLevel(i);
             }
-
-
         }
-        GUILayout.EndScrollView(); 
+
+        GUILayout.EndScrollView();
+
         #endregion
     }
+
     #endregion
 
     #region DrawItem
     private void DrawItem(int index)
     {
         GUILayout.BeginHorizontal(ItemStyle);
-        
-        if (_boundText.Count == 0)
+
+        var memberType = "";
+
+        switch (_memberInfos[index].MemberType)
         {
-            GUILayout.Label(_types[index].Name, ItemLableStyle, GUILayout.Width(200));
-            GUILayout.Label($"({_types[index].FullName})", ItemLableStyle);
+            case MemberTypes.Field:
+                var fieldInfo = (FieldInfo)_memberInfos[index];
+
+                memberType = $"{fieldInfo.FieldType.Name}";
+                break;
+
+            case MemberTypes.Property:
+                var propertyInfo = _memberInfos[index] as PropertyInfo;
+
+                memberType = $"{propertyInfo.PropertyType.Name}";
+                break;
+
+            case MemberTypes.Method:
+                var methodInfo = _memberInfos[index] as MethodInfo;
+
+                memberType = $"{methodInfo.ReturnType.Name}(";
+
+                var parameters = methodInfo.GetParameters();
+
+                for (var i = 0; i < parameters.Length; i++)
+                {
+                    var parameterInfo = parameters[i];
+                    memberType +=
+                        $"{parameterInfo.ParameterType.Name} {parameterInfo.Name}{(i == parameters.Length - 1 ? "" : ",  ")} ";
+                }
+
+                memberType += ")";
+                break;
+
+            default:
+                throw new ArgumentOutOfRangeException();
         }
-        else
-        {
-            var lable1 = "";
-            var lable2 = "";
 
-            switch (_memberInfos[index].MemberType)
-            {
-                case MemberTypes.Field:
-                    var fieldInfo = (FieldInfo)_memberInfos[index];
-
-                    lable1 = fieldInfo.Name;
-                    lable2 = $"{fieldInfo.FieldType.Name}";
-                    break;
-
-                case MemberTypes.Property:
-                    var propertyInfo = _memberInfos[index] as PropertyInfo;
-
-                    lable1 = propertyInfo.Name;
-                    lable2 = $"{propertyInfo.PropertyType.Name}";
-                    break;
-
-                case MemberTypes.Method:
-                    var methodInfo = _memberInfos[index] as MethodInfo;
-
-                    lable1 = methodInfo.Name;
-                    lable2 = $"{methodInfo.ReturnType.Name}(";
-
-                    var parameters = methodInfo.GetParameters();
-
-                    for (var i = 0; i < parameters.Length; i++)
-                    {
-                        var parameterInfo = parameters[i];
-                        lable2 += $"{parameterInfo.ParameterType.Name} {parameterInfo.Name}{(i == parameters.Length - 1 ? "" : ",  ")} ";
-                    }
-
-                    lable2 += ")";
-                    break;
-
-                default:
-                    throw new ArgumentOutOfRangeException();
-            }
-
-            GUILayout.Label(lable1, ItemLableStyle, GUILayout.Width(200));
-            GUILayout.Label(_memberInfos[index].DeclaringType.Name, ItemLableStyle, GUILayout.Width(200));
-            GUILayout.Label(lable2, ItemLableStyle);
-        }
+        GUILayout.Label(_memberInfos[index].Name, ItemLableStyle, GUILayout.Width(200));
+        GUILayout.Label(_memberInfos[index].DeclaringType.Name, ItemLableStyle, GUILayout.Width(200));
+        GUILayout.Label(memberType, ItemLableStyle);
         GUILayout.EndHorizontal();
     }
 
