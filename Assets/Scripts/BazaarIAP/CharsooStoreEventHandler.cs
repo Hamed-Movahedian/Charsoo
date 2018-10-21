210404af8f5b440da1e16d2cab4ec222b29ba6b2
HoseinNajafi <najafi.h.ir@gmail.com>
2018-10-21 22:38:47 +0330

    no message

--- Assets/Scripts/BazaarIAP/CharsooStoreEventHandler.cs
+++ Assets/Scripts/BazaarIAP/CharsooStoreEventHandler.cs
@@ -10,8 +10,12 @@ namespace Soomla.Store.Charsoo
         public CharsooStoreEventHandler()
         {
             StoreEvents.OnMarketPurchase += OnMarketPurchase;
-            StoreEvents.OnItemPurchased += OnItemPurchased;
+            StoreEvents.OnGoodUpgrade += GoodUpgrade;
+            StoreEvents.OnGoodEquipped += GoodEq;
+            StoreEvents.OnGoodBalanceChanged += OnGoodPurchased;
+            StoreEvents.OnGoodUnEquipped += OnGoodUnEq;
             StoreEvents.OnCurrencyBalanceChanged += OnCurrencyBalanceChanged;
+            StoreEvents.OnItemPurchased += OnItem;
             //Singleton.Instance.DebugerText.text = StoreInventory.GetItemBalance("chasoo_currency").ToString();
 #if UNITY_ANDROID && !UNITY_EDITOR
 			StoreEvents.OnIabServiceStarted += onIabServiceStarted;
@@ -19,14 +23,35 @@ namespace Soomla.Store.Charsoo
 #endif
         }
 
-        public void OnMarketPurchase(PurchasableVirtualItem pvi, string payload, Dictionary<string, string> extra)
+        private void OnItem(PurchasableVirtualItem arg1, string arg2)
         {
-
+            Debug.Log(arg1);
         }
 
-        public void OnItemPurchased(PurchasableVirtualItem pvi, string payload)
+        private void OnGoodUnEq(EquippableVG obj)
         {
-            string pviItemId = pvi.ItemId;
+            Debug.Log(obj);
+        }
+
+        private void GoodEq(EquippableVG obj)
+        {
+            Debug.Log(obj);
+        }
+
+        private void GoodUpgrade(VirtualGood arg1, UpgradeVG arg2)
+        {
+
+            Debug.Log(arg1);
+        }
+
+        public void OnMarketPurchase(PurchasableVirtualItem pvi, string payload, Dictionary<string, string> extra)
+        {
+            Debug.Log(pvi);
+        }
+
+        public void OnGoodPurchased(VirtualGood virtualGood, int balance, int amountAdded)
+        {
+            string pviItemId = virtualGood.ItemId;
             if (pviItemId.Contains("_CoinsPack"))
                 return;
 
@@ -35,7 +60,8 @@ namespace Soomla.Store.Charsoo
                 var playerController = Singleton.Instance.PlayerController;
                 PlayerInfo playerInfo = playerController.PlayerInfo;
                 playerInfo.HasDubler = true;
-                
+                Singleton.Instance.PlayerController.ChangePlayerInfo(playerInfo);
+
             }
         }
 
