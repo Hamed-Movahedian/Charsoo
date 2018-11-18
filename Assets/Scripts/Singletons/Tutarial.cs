af3816e5b243875e736cf0b2bce2a31f646e029b
HoseinNajafi <najafi.h.ir@gmail.com>
2018-11-18 17:22:41 +0330

    no message

--- Assets/Scripts/Singletons/Tutarial.cs
+++ Assets/Scripts/Singletons/Tutarial.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using FMachine;
 using FollowMachineDll.Attributes;
+using MgsCommonLib.Animation;
 using UnityEngine;
 
 public class Tutarial : MonoBehaviour
@@ -10,16 +11,53 @@ public class Tutarial : MonoBehaviour
     private int _puzzleIndex = 0;
     private Puzzle _puzzle;
     public HUD HUD;
+    public int CurrentWord = 0;
+    private bool _break = false;
+    private List<Vector3> _letterPos;
 
+
+    [FollowMachine("Prepare next puzzle for spawn", "Play Next,No Next Puzzle")]
     public void SetPuzzle()
     {
         _puzzle = LocalDBController
             .Table<Puzzle>()
-            .SqlWhere(p => p.CategoryID == 2044 && p.Row == _puzzleIndex)
-            .Single();
-
+            .FirstOrDefault(p => p.CategoryID == 2044 && p.Row == _puzzleIndex);
+        if (_puzzle==null)
+        {
+            FollowMachine.SetOutput("No Next Puzzle");
+            return;
+        }
+        
         SetForSpawn(_puzzle);
+
+        _puzzleIndex++;
+
+        if (Singleton.Instance.WordSpawner.PuzzleReward)
+            ZPlayerPrefs.SetInt("LastPlayedPuzzle", _puzzle.ID);
+
+        FollowMachine.SetOutput("Play Next");
+
     }
+
+    [FollowMachine("Prepare next puzzle for spawn", "Play Next,No Next Puzzle")]
+    public void PuzzleSolved()
+    {
+
+
+        var puzzles = LocalDBController.Table<Puzzle>().
+            SqlWhere(p => p.CategoryID == _puzzle.CategoryID);
+
+        Puzzle nextPuzzle = puzzles.FirstOrDefault(p => p.Row == _puzzleIndex);
+
+        if (nextPuzzle == null || nextPuzzle.Solved)
+        {
+            FollowMachine.SetOutput("No Next Puzzle");
+            return;
+        }
+
+        FollowMachine.SetOutput("Play Next");
+    }
+    
     public void SetForSpawn(Puzzle selectedPuzzle)
     {
         WordSet wordSet = selectedPuzzle.GetWordSet();
@@ -32,18 +70,72 @@ public class Tutarial : MonoBehaviour
 
         Singleton.Instance.WordSpawner.EditorInstatiate = null;
     }
+
     public void SetupHUD()
     {
-        HUD.StartClueText.text = _puzzleIndex == 0? PersianFixer.Fix("به چارسو \n خوش آمدید"): PersianFixer.Fix(_puzzle.Clue);
+        HUD.StartClueText.text = _puzzleIndex == 0 ? PersianFixer.Fix("به چارسو \n خوش آمدید") : PersianFixer.Fix(_puzzle.Clue);
         HUD.HeaderClueText.text = PersianFixer.Fix(_puzzle.Clue);
         HUD.HintClueText.text = PersianFixer.Fix(_puzzle.Clue);
-        HUD.IndexText.text = PersianFixer.Fix((_puzzle.Row+1).ToString(),true,true);
+        HUD.IndexText.text = PersianFixer.Fix((_puzzle.Row + 1).ToString(), true, true);
     }
 
 
-    [FollowMachine("Check Tutorial Index","0,1,2")]
+    [FollowMachine("Check Word Index", "Break,NextWord,Continue")]
+    public void CheckWord()
+    {
+        if (_puzzleIndex >1)
+        {
+            FollowMachine.SetOutput("Continue");
+            return;
+        }
+
+        Word currentWord = Singleton
+            .Instance
+            .WordManager
+            .Words[CurrentWord];
+        Debug.Log(currentWord.Name);
+
+        bool correctWord = currentWord
+            .IsComplete;
+
+        if (_break || !correctWord)
+        {
+            FollowMachine.SetOutput("Break");
+            _break = true;
+            return;
+        }
+        CurrentWord++;
+        FollowMachine.SetOutput("NextWord");
+    }
+
+    public void SaveLetterPositions()
+    {
+        _letterPos = Singleton.Instance.LetterController.AllLetters
+            .Select(l => l.transform.position)
+            .ToList();
+    }
+
+    public IEnumerator ResetLettersToSaveLocaion(float duration)
+    {
+
+        var letters = Singleton.Instance.LetterController.AllLetters;
+
+        var startPos = letters.Select(l=>l.transform.position).ToList();
+
+        yield return MsgAnimation.RunAnimation(
+            duration,
+            v =>
+            {
+                for (int i = 0; i < letters.Count; i++)
+                {
+                    letters[i].transform.position = Vector3.Lerp(startPos[i], _letterPos[i], v);
+                }
+            });
+    }
+
+    [FollowMachine("Check Tutorial Index", "0,1,2,Other")]
     public void TutorialState()
     {
-        FollowMachine.SetOutput(_puzzleIndex.ToString());
+        FollowMachine.SetOutput(_puzzleIndex<4?(_puzzleIndex - 1).ToString():"Other");
     }
 }
