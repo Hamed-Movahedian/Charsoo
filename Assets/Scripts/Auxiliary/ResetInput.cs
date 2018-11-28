c0530d688c267a17763a74abe0f1e749b2155a73
HoseinNajafi <najafi.h.ir@gmail.com>
2018-11-28 20:25:07 +0330

    no message

---
+++ Assets/Scripts/Auxiliary/ResetInput.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ResetInput : MonoBehaviour
+{
+    private InputField _field;
+
+    public void ResetText()
+    {
+        _field = GetComponent<InputField>();
+
+        if (_field != null)
+            _field.text = "";
+    }
+}
